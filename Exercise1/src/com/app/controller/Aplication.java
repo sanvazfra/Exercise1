d2f94ff5f1cf3a38cef468f419bfc4eab40c8de3
sanvazfra <f_1_90@hotmail.com>
2015-03-21 01:05:35 -0600

    Agregue el metodo tengo ambre!!!!

--- Exercise1/src/com/app/controller/Aplication.java
+++ Exercise1/src/com/app/controller/Aplication.java
@@ -4,5 +4,9 @@ public class Aplication {
 	public static void main(String  args[]){
 		System.out.println("Hola mundo!");
 		System.out.println("Hola mundo!");
+		tengoAmbre();
+	}
+	public static void tengoAmbre(){
+		System.out.println("Tengo ambre!!!");
 	}
 }
